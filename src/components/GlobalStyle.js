8d9c20994fd3d3b48c8f58b270af56db9533c31f
ism90 <ianmason5@gmail.com>
2021-12-06 23:57:11 +0000

    Build

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -2,10 +2,6 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
-@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap");
-
-
-
 * {
   margin: 0;
   padding: 0;
