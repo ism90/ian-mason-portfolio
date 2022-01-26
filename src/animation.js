0f71337edf7f5eca7b71dd22414ed44bf163a0b5
ism90 <ianmason5@gmail.com>
2022-01-26 13:32:55 +0000

    links to code and live on projects

--- src/animation.js
+++ src/animation.js
@@ -33,3 +33,15 @@ export const fade = {
   hidden: { opacity: 0 },
   show: { opacity: 1, transition: { ease: "easeOut", duration: 0.75 } },
 };
+
+export const photoAnimation = {
+  hidden: { scale: 1.5, opacity: 0 },
+  show: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.75,
+    },
+  },
+};
--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -5,7 +5,7 @@ import styles from "./ProjectSection.module.scss";
 import { Link } from "react-router-dom";
 
 import { motion } from "framer-motion";
-import { pageAnimation } from "../../animation";
+import { pageAnimation, fade, photoAnimation } from "../../animation";
 
 import Lujam from "../../assets/images/Lujam.png";
 import ConnectFour from "../../assets/images/ConnectFour.png";
@@ -23,10 +23,24 @@ const ProjectsSection = () => {
       className={styles.projects}
     >
       <div className={styles.project}>
-        <h2>LUJAM - Client Project</h2>
-        <div className={styles.line}></div>
+        <motion.div variants={fade} className={styles.title}>
+          {" "}
+          <h2>LUJAM - Client Project </h2>{" "}
+          <div>
+            <a href="https://lujam-jersey.web.app/">
+              <button className={styles.button}>Live</button>
+            </a>
+            <a href="https://github.com/ism90/jersey-client-project">
+              <button className={styles.button}>Code</button>
+            </a>
+          </div>
+        </motion.div>
+        <motion.div className={styles.line}></motion.div>
         <Link to="/ian-mason-portfolio/projects/lujam">
-          <img src={Lujam} alt="Lujam" />
+          <div className={styles.imgWrapper}>
+            <motion.img variants={photoAnimation} src={Lujam} alt="Lujam" />
+            <div className={styles.content}> Details</div>
+          </div>
         </Link>
         <ul className={styles.list}>
           <li>React</li>
@@ -36,11 +50,28 @@ const ProjectsSection = () => {
           <li>Agile</li>
         </ul>
       </div>
+
       <div className={styles.project}>
-        <h2>Connect Four!</h2>
-        <div className={styles.line}></div>
+        <motion.div variants={fade} className={styles.title}>
+          <h2> Connect Four! </h2>
+          <div>
+            <a href="https://lujam-jersey.web.app/">
+              <button className={styles.button}>Live</button>
+            </a>
+            <a href="https://github.com/ism90/jersey-client-project">
+              <button className={styles.button}>Code</button>
+            </a>
+          </div>
+        </motion.div>
+        <motion.div className={styles.line}></motion.div>
         <Link to="/ian-mason-portfolio/projects/connect-four">
-          <img src={ConnectFour} alt="Connect Four" />
+          <div className={styles.imgWrapper}>
+            <motion.img
+              variants={photoAnimation}
+              src={ConnectFour}
+              alt="Connect Four"
+            />
+          </div>
         </Link>
         <ul className={styles.list}>
           <li>HTML</li>
@@ -48,11 +79,24 @@ const ProjectsSection = () => {
           <li>JavaScript</li>
         </ul>
       </div>
+
       <div className={styles.project}>
-        <h2>Punk API</h2>
-        <div className={styles.line}></div>
+        <motion.div variants={fade} className={styles.title}>
+          <h2> Punk API </h2>
+          <div>
+            <a href="https://lujam-jersey.web.app/">
+              <button className={styles.button}>Live</button>
+            </a>
+            <a href="https://github.com/ism90/jersey-client-project">
+              <button className={styles.button}>Code</button>
+            </a>
+          </div>
+        </motion.div>
+        <motion.div className={styles.line}></motion.div>
         <Link to="/ian-mason-portfolio/projects/punk-api">
-          <img src={Punk} alt="Punk API" />
+          <div className={styles.imgWrapper}>
+            <motion.img variants={photoAnimation} src={Punk} alt="Punk API" />
+          </div>
         </Link>
         <ul className={styles.list}>
           <li>React</li>
@@ -60,27 +104,63 @@ const ProjectsSection = () => {
           <li>JSX</li>
         </ul>
       </div>
+
       <div className={styles.project}>
-        <h2>JS Calculator</h2>
-        <div className={styles.line}></div>
+        <motion.div variants={fade} className={styles.title}>
+          <h2> JS Calculator </h2>
+          <div>
+            <a href="https://lujam-jersey.web.app/">
+              <button className={styles.button}>Live</button>
+            </a>
+            <a href="https://github.com/ism90/jersey-client-project">
+              <button className={styles.button}>Code</button>
+            </a>
+          </div>
+        </motion.div>
+        <motion.div className={styles.line}></motion.div>
         <Link to="/ian-mason-portfolio/projects/js-calculator">
-          <img src={Calc} alt="Calculator" />
+          <div className={styles.imgWrapper}>
+            <motion.img variants={photoAnimation} src={Calc} alt="Calculator" />
+          </div>
         </Link>
+        <ul className={styles.list}>
+          <li>HTML</li>
+          <li>SCSS</li>
+          <li>JavaScript</li>
+          <li>E2E Testing</li>
+          <li>Cypress</li>
+        </ul>
       </div>
+
       <div className={styles.project}>
-        <h2>Morse Code Translator</h2>
-        <div className={styles.line}></div>
+        <motion.div variants={fade} className={styles.title}>
+          <h2> Morse Code Translator </h2>
+          <div>
+            <a href="https://lujam-jersey.web.app/">
+              <button className={styles.button}>Live</button>
+            </a>
+            <a href="https://github.com/ism90/jersey-client-project">
+              <button className={styles.button}>Code</button>
+            </a>
+          </div>
+        </motion.div>
+        <motion.div className={styles.line}></motion.div>
         <Link to="/ian-mason-portfolio/projects/morse-code-translator">
-          <img src={Morse} alt="Morse Code Translator" />
+          <div className={styles.imgWrapper}>
+            <motion.img
+              variants={photoAnimation}
+              src={Morse}
+              alt="Morse Code Translator"
+            />
+          </div>
         </Link>
+        <ul className={styles.list}>
+          <li>HTML</li>
+          <li>SCSS</li>
+          <li>JavaScript</li>
+          <li>TDD</li>
+        </ul>
       </div>
-      {/* <div className={styles.project}>
-        <h2>JS Calculator</h2>
-        <div className={styles.line}></div>
-        <Link to="/ian-mason-portfolio/projects/connect-four">
-          <img src={ConnectFour} alt="Connect Four" />
-        </Link>
-      </div> */}
     </motion.div>
   );
 };
