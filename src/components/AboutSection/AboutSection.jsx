4c0bbfa0878fe772dba48367f00789a790239fc3
ism90 <ianmason5@gmail.com>
2022-01-31 15:47:59 +0000

    about page responsive

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -23,15 +23,24 @@ const AboutSection = () => {
         <div className={styles.hide}>
           <motion.h2 variants={fade}>About</motion.h2>
         </div>
+        <div className={styles.contents}>
+          <motion.p variants={fade}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </motion.p>
 
-        <motion.p variants={fade}>
-          I have recently completed a 12 week... (tbc)
-        </motion.p>
-
-        <Link to="ian-mason-portfolio/contact">
-          {" "}
-          <motion.button variants={fade}>Contact</motion.button>{" "}
-        </Link>
+          <Link to="ian-mason-portfolio/contact">
+            {" "}
+            <motion.button variants={fade}>Contact</motion.button>{" "}
+          </Link>
+        </div>
       </div>
       <div className={styles.image}>
         <motion.img variants={fade} src={Me} alt="Ian" />
