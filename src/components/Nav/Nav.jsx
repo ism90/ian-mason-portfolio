c54ce76102d78c3bf3fd9df98f1c0754db6085a0
ism90 <ianmason5@gmail.com>
2022-02-01 12:34:21 +0000

    circle anim for nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,8 +3,12 @@ import Logo from "../../assets/images/Logo.png";
 import { Link } from "react-router-dom";
 
 import styles from "./Nav.module.scss";
+import { motion } from "framer-motion";
+
+import { useLocation } from "react-router-dom";
 
 const Nav = () => {
+  const { pathname } = useLocation();
   return (
     <nav className={styles.nav}>
       <h1>
@@ -14,23 +18,61 @@ const Nav = () => {
       </h1>
       <ul className={styles.ul}>
         <li className={styles.li}>
-          <Link to="ian-mason-portfolio/projects" className={styles.link}>Projects</Link>
-        </li>
-        <li className={styles.li}>
-          <Link to="ian-mason-portfolio/about" className={styles.link}>About</Link>
-        </li>
-        <li className={styles.li}>
-          <Link to="ian-mason-portfolio/contact" className={styles.link}>Contact</Link>
-        </li>
-        <li className={styles.li}>
-          <Link to="/ian-mason-portfolio" className={styles.resume}>CV</Link>
+          <Link to="ian-mason-portfolio/projects" className={styles.link}>
+            Projects
+          </Link>
+          <motion.div
+            className={styles.line}
+            transition={{ duration: 0}}
+            initial={{ width: "0%" }}
+            animate={{
+              width: pathname.startsWith("/ian-mason-portfolio/projects")
+                ? "5px"
+                : "0%",
+            }}
+          ></motion.div>
         </li>
 
+        <li className={styles.li}>
+          <Link to="ian-mason-portfolio/about" className={styles.link}>
+            About
+          </Link>
+          <motion.div
+            className={styles.line}
+
+            initial={{ width: "0%" }}
+            animate={{
+              width: pathname.startsWith("/ian-mason-portfolio/about")
+                ? "5px"
+                : "0%",
+            }}
+          ></motion.div>
+        </li>
+
+        <li className={styles.li}>
+          <Link to="ian-mason-portfolio/contact" className={styles.link}>
+            Contact
+          </Link>
+          <motion.div
+            className={styles.line}
+            transition={{ duration: 0 }}
+            initial={{ width: "0%" }}
+            animate={{
+              width: pathname.startsWith("/ian-mason-portfolio/contact")
+                ? "5px"
+                : "0%",
+            }}
+          ></motion.div>
+        </li>
+
+        <li className={styles.li}>
+          <Link to="/ian-mason-portfolio" className={styles.resume}>
+            CV
+          </Link>
+        </li>
       </ul>
     </nav>
   );
 };
-
-
 
 export default Nav;
