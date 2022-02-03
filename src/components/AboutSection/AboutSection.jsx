4f341961d70eeaa141d1e352286cf2911539a931
ism90 <ianmason5@gmail.com>
2022-02-03 12:21:04 +0000

    tech icons added to about

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -7,6 +7,16 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { pageAnimation, fade } from "../../animation";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHtml5,
+  faCss3Alt,
+  faJs,
+  faReact,
+  faSass,
+  faJava,
+} from "@fortawesome/free-brands-svg-icons";
+
 import Me from "../../assets/images/Me.png";
 import Wave from "../Wave/Wave";
 
@@ -25,16 +35,25 @@ const AboutSection = () => {
         </div>
         <div className={styles.contents}>
           <motion.p variants={fade}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            I'm Ian, a Junior Dev based in London, UK. I have recently completed
+            the 12-week Software Developer Course with{" "}
+            <a href="https://nology.io/"><span>_nology.</span></a>
           </motion.p>
+          <div className={styles.hide}>
+            <motion.h2 variants={fade}> Stack</motion.h2>
+          </div>
+          <motion.div className={styles.grid}>
+            <FontAwesomeIcon icon={faHtml5} size="6x" className={styles.icon} />
+            <FontAwesomeIcon
+              icon={faCss3Alt}
+              size="6x"
+              className={styles.icon}
+            />
+            <FontAwesomeIcon icon={faJs} size="6x" className={styles.icon} />
+            <FontAwesomeIcon icon={faReact} size="6x" className={styles.icon} />
+            <FontAwesomeIcon icon={faSass} size="6x" className={styles.icon} />
+            <FontAwesomeIcon icon={faJava} size="6x" className={styles.icon} />
+          </motion.div>
 
           <Link to="ian-mason-portfolio/contact">
             {" "}
